b95319a8c53cd3145aee88db757110b0b4f28a42
Kackestampferkarl <145342494+Kackestampferkarl@users.noreply.github.com>
2023-12-13 17:42:28 +0100

    Initial commit

---
+++ tvscreen.js
@@ -0,0 +1,9 @@
+let power = function () {
+    let body = document.getElementById("wrapper");
+    body.className = (body.className == "on") ? "off" : "on";
+}
+
+let reversePower = function () {
+    let body = document.getElementById("wrapper");
+    body.className = (body.className == "on") ? "off" : "on";
+}
